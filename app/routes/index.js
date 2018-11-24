e7d41f04591113bc8b8e5450665a1c18778088fa
bikashprusty <bikash.sandbox@gmail.com>
2018-11-24 14:57:13 +0530

    Ember task1 upload

---
+++ app/routes/index.js
@@ -0,0 +1,16 @@
+import Route from '@ember/routing/route'; 
+
+export default Route.extend({
+  model() {
+    return this.store.findAll('question');
+  },
+
+  actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store
+                 .createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+  }
+});
